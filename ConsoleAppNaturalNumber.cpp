8f043a95492ad594dc65e48bf8ebcda1968b07bd
cvv_code <cvv_code@gmail.com>
2024-01-20 13:51:52 +0500

    Добавьте файлы проекта.

---
+++ ConsoleAppNaturalNumber.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int naturalNumber = 0;
+	while (naturalNumber <= 0)
+	{
+		std::cout << "Введите натуральное число: ";
+		std::cin >> naturalNumber;
+	}
+
+	int sum = 0;
+	int pro = 1;
+	do
+	{
+		sum += naturalNumber % 10;
+		pro *= naturalNumber % 10;
+		naturalNumber /= 10;
+	} while (naturalNumber != 0);
+
+	std::cout << "Сумма = " << sum << "\nПроизведение = " << pro;
+}
